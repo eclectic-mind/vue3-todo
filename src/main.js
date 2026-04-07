564483e52f483b3d26b0de9b039357456d79fc0b
eclectic-mind <maria.korolkova@gmail.com>
2026-04-07 13:12:53 +0300

    init commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+const pinia = createPinia()
+app.use(pinia)
 
 app.mount('#app')
